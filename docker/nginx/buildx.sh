b1032943b99969e4fdc5074c0d6694ac4bae509b
tangweixin <thomastangweixin@163.com>
2025-09-17 02:37:52 +0800

    nginx:1.24.0

--- docker/nginx/buildx.sh
+++ docker/nginx/buildx.sh
@@ -4,8 +4,8 @@ cd `dirname $0` && pwd
 
 echo '开始构建镜像'
 
-docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.28.0-alpine --push -f Dockerfile-alpine .
-docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.28.0 --push -f Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.24.0-alpine --push -f Dockerfile-alpine .
+docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.24.0 --push -f Dockerfile .
 
 # 本地构建镜像并且测试
 #COMPOSE_BAKE=true docker-compose build httpsok-nginx-alpine
