32c8de15a59bd0c2907bf939a9cfae7b9a22bda8
tangweixin <thomastangweixin@163.com>
2025-09-17 00:59:40 +0800

    基于nginx官方镜像制作自带httpsok自动续签的镜像

---
+++ docker/nginx/buildx.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+cd `dirname $0` && pwd
+
+echo '开始构建镜像'
+
+docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.28.0-alpine --push -f Dockerfile-alpine .
+docker buildx build --platform linux/amd64,linux/arm64 -t httpsok/nginx:1.28.0 --push -f Dockerfile .
+
+# 本地构建镜像并且测试
+#COMPOSE_BAKE=true docker-compose build httpsok-nginx-alpine
+
